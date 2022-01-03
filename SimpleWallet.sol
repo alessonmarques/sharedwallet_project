08fe467d9eddadffb8490781019c9bd55ddd47ca
Alesson Marques <alessonmarques@live.com>
2022-01-03 02:44:22 -0300

    Setting up events to check the money sending/receiving.

--- SimpleWallet.sol
+++ SimpleWallet.sol
@@ -25,16 +25,20 @@ contract Allowance is Ownable {
 
 contract SimpleWallet is Allowance {
 
+  event MoneySent(address indexed _beneficiary, uint _amount);
+  event MoneyReceived(address indexed _from, uint _amount);
+
   function withdrawMoney(address payable _to, uint _amount) public isAllowed(_amount) {
     require(_amount <= address(this).balance, "There are not enough funds stroed in this contract.");
     if (!isOwner()) {
       reduceAllowance(msg.sender, _amount);
     }
+    emit MoneySent(_to, _amount);
     _to.transfer(_amount);
   }
 
   function () external payable {
-
+    emit MoneyReceived(msg.sender, msg.value);
   }
 
 }
