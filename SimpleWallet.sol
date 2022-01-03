be3b19f4c65ee69bfd9828a711d4c3600b588d9a
Alesson Marques <alessonmarques@live.com>
2022-01-03 02:39:23 -0300

    Setting up events to check allowance.

--- SimpleWallet.sol
+++ SimpleWallet.sol
@@ -2,11 +2,12 @@ pragma solidity ^0.5.13;
 
 import "Ownable.sol";
 
-contract SimpleWallet is Ownable {
-
+contract Allowance is Ownable {
+  event AllowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount, uint _newAmount);
   mapping (address => uint) public allowance;
 
   function addAllowance(address _who, uint _amount) public onlyOwner {
+    emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
     allowance[_who] = _amount;
   }
 
@@ -16,8 +17,13 @@ contract SimpleWallet is Ownable {
   }
 
   function reduceAllowance(address _who, uint _amount) internal {
+    emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
     allowance[_who] -= _amount;
   }
+
+}
+
+contract SimpleWallet is Allowance {
 
   function withdrawMoney(address payable _to, uint _amount) public isAllowed(_amount) {
     require(_amount <= address(this).balance, "There are not enough funds stroed in this contract.");
