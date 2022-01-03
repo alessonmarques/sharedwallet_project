06b9fad56b193058e5785a03e8135aedbeb42536
Alesson Marques <alessonmarques@live.com>
2022-01-03 02:20:05 -0300

    Setting the allowance and new modifier to allow withdraw.

--- SimpleWallet.sol
+++ SimpleWallet.sol
@@ -4,7 +4,18 @@ import "Ownable.sol";
 
 contract SimpleWallet is Ownable {
 
-  function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+  mapping (address => uint) public allowance;
+
+  function addAllowance(address _who, uint _amount) public onlyOwner {
+    allowance[_who] = _amount;
+  }
+
+  modifier isAllowed(uint _amount) {
+    require(isOwner() || allowance[msg.sender] >= _amount, "You are now allowed to do this.");
+    _;
+  }
+
+  function withdrawMoney(address payable _to, uint _amount) public isAllowed(_amount) {
     _to.transfer(_amount);
   }
 
