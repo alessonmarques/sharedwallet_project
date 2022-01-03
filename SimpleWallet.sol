ee5b6b2680fddb1248c64f1797a03467147cb9d2
Alesson Marques <alessonmarques@live.com>
2022-01-03 02:51:18 -0300

    Adding the SafeMath methods to keep it safe and overwriting the sub methods.

--- SimpleWallet.sol
+++ SimpleWallet.sol
@@ -1,24 +1,29 @@
 pragma solidity ^0.5.13;
 
 import "Ownable.sol";
+import "SafeMath.sol";
 
 contract Allowance is Ownable {
-  event AllowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount, uint _newAmount);
-  mapping (address => uint) public allowance;
+  
+  using SafeMath for uint;
 
-  function addAllowance(address _who, uint _amount) public onlyOwner {
-    emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
-    allowance[_who] = _amount;
-  }
+  event AllowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount, uint _newAmount);
+
+  mapping (address => uint) public allowance;
 
   modifier isAllowed(uint _amount) {
     require(isOwner() || allowance[msg.sender] >= _amount, "You are now allowed to do this.");
     _;
   }
 
+  function addAllowance(address _who, uint _amount) public onlyOwner {
+    emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
+    allowance[_who] = _amount;
+  }
+
   function reduceAllowance(address _who, uint _amount) internal {
-    emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
-    allowance[_who] -= _amount;
+    emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who].sub(_amount));
+    allowance[_who] = allowance[_who].sub(_amount);
   }
 
 }
